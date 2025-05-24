c838a65c232ce0e6f3c615d216cd2c1217471ce2
luz <carineortega9@gmail.com>
2025-05-24 16:04:26 -0500

    Refactor CallbackComponent to enhance user data handling and integrate SharedDataService for state management

--- src/app/auth/callback/callback.component.ts
+++ src/app/auth/callback/callback.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { HttpClient } from '@angular/common/http';
+import { EmprendimientoService } from '../../app-core/servicios/emprendimiento.service';
+import { jwtDecode } from 'jwt-decode';
+import { SharedDataService } from '../../app-core/servicios/shared-data.service';
 
 
 @Component({
@@ -15,9 +18,19 @@ export class CallbackComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private http: HttpClient,
-    private authService: AuthService,
-    private router: Router
+    private AuthService: AuthService,
+    private router: Router,
+    private emprendimientoService: EmprendimientoService,
+    private userStateService: SharedDataService
   ) { }
+
+  usuario: any[] = [];
+  identificacion: any[] = [];
+  ids: any[] = [];
+  idUser: string = '';
+  username: string = '';
+  role: string = '';
+  roles: string[] = [];
 
   ngOnInit(): void {
 
@@ -30,11 +43,14 @@ export class CallbackComponent implements OnInit {
           const idToken = response.id_token;
 
           // Almacena los tokens de forma síncrona
-          this.authService.setToken(token);
-          this.authService.setIdToken(idToken);
+          this.AuthService.setToken(token);
+          this.AuthService.setIdToken(idToken);
+          this.obtenerUsuario();
           // console.log('Token received:', token);
           // console.log('IdToken received:', idToken);
-          
+
+
+
           this.router.navigate(['/dashboard']);
 
         },
@@ -44,5 +60,44 @@ export class CallbackComponent implements OnInit {
         }
       });
     }
+  }
+
+  obtenerUsuario() {
+    this.emprendimientoService.obtenerUsuarioKey().subscribe(data => {
+      this.userStateService.setUsuario(data);
+      this.usuario = data;
+
+      const token = this.AuthService.getToken();
+      if (token) {
+        const decodedToken: any = jwtDecode(token);
+        const sub = decodedToken.sub;
+        this.emprendimientoService.obtenerUsuarioKeyPorId(sub).subscribe(data => {
+          this.userStateService.setIdUser(data.identificacion);
+          this.userStateService.setUsername(data.username);
+          this.userStateService.setRole(data.role);
+
+          this.idUser = data.identificacion;
+          this.username = data.username;
+          this.role = data.role;
+
+          if (decodedToken.realm_access?.roles) {
+            const technicalRoles = ['offline_access', 'uma_authorization', 'default-roles-laravel-realm'];
+            const filteredRoles = decodedToken.realm_access.roles.filter((role: string) =>
+              !technicalRoles.includes(role)
+            );
+            this.userStateService.setRoles(filteredRoles);
+            this.role = filteredRoles[0] || 'sin-rol';
+            this.userStateService.setRole(this.role);
+          }
+
+          if (this.idUser !== null) {
+            // Datos válidos
+          } else {
+            this.idUser = '0';
+            console.error('No se encontró el usuario logueado en la lista de usuarios');
+          }
+        });
+      }
+    });
   }
 }
