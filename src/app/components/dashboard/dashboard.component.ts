8b8bc4e4ba867e2f2e8156c1ecbefbd052925376
luz <carineortega9@gmail.com>
2025-05-26 20:06:21 -0500

    Refactor dashboard component structure and styles; add sidebar and content layout

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 import { JlayaoutComponent } from '../../containers/jlayaout/jlayaout/jlayaout.component';
-
+import { MenuEmprendedorComponent } from '../../containers/jlayaout/menu-emprendedor/menu-emprendedor.component';
+import { FormEmprendimientoComponent } from '../emprendimiento/form-emprendimiento/form-emprendimiento.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-dashboard',
-  imports: [JlayaoutComponent],
+  imports: [JlayaoutComponent, MenuEmprendedorComponent, FormEmprendimientoComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-
 export class DashboardComponent {
+  selectedOption: string = '';
   constructor(private authService: AuthService) { 
     const token = this.authService.getToken();
   }
 }
+
+
+
+
